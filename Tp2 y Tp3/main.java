39feee61fb5a83561572d0af99392c218043f603
TommyMont <tmmymont@gmail.com>
2025-03-30 20:41:14 -0300

    Add files via upload

---
+++ Tp2 y Tp3/main.java
@@ -0,0 +1,121 @@
+package javaclass;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ejercicio1 --------------------------------------------------------");
+//        int anio = scanner.nextInt();
+//        if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+//            System.out.println("Es bisiesto");
+//        } else {
+//            System.out.println("No es bisiesto");
+//        }
+        System.out.println("Ejercicio2 --------------------------------------------------------");
+//        int num1 = scanner.nextInt();
+//        int num2 = scanner.nextInt();
+//        int num3 = scanner.nextInt();
+//        int Resultado = 0;
+//        if (num1 > num2 && num1 > num3) {
+//            Resultado = num1;
+//        } else if (num2 > num1 && num2 > num3) {
+//            Resultado = num2;
+//
+//        } else if (num3 > num1 && num3 > num2){
+//            Resultado = num3;
+//
+//    }else
+//
+//    {
+//        System.out.println("Son iguales");
+//    }{
+//        System.out.println(Resultado);
+//        }
+        System.out.println("Ejercicio4 --------------------------------------------------------");
+
+//        int Edad = scanner.nextInt();
+//
+//        if (Edad < 12 ){
+//            System.out.println("Eres un niño");
+//        }else if (Edad >= 12 & Edad <= 17){
+//            System.out.println("Eres un adolecente");
+//        }else if (Edad >= 18 & Edad <= 59){
+//            System.out.println("Eres un adulto");
+//        }else if (Edad >= 60){
+//            System.out.println("Eres un adulto mayor");
+//   }
+        System.out.println("Ejercicio4 --------------------------------------------------------");
+//
+//        System.out.println("Definir precio");
+//            int Precio = scanner.nextInt();
+//            int Descuento = 0;
+//        System.out.println("Definir categoria");
+//            String Categoria = scanner.next();
+//
+//            int A = 10;
+//            int B = 15;
+//            int C = 20;
+//
+//        if (Categoria.equals("A")) {
+//            Descuento = Precio - (int) (Precio * (A / 100.0));
+//            System.out.println("Descuento aplicado: 10%");
+//        } else if (Categoria.equals("B")) {
+//            Descuento = Precio - (int) (Precio * (B / 100.0));
+//            System.out.println("Descuento aplicado: 15%");
+//        } else if (Categoria.equals("C")) {
+//            Descuento = Precio - (int) (Precio * (C / 100.0));
+//            System.out.println("Descuento aplicado: 20%");
+//        } else {
+//            System.out.println("Categoría no válida, sin descuento.");
+//        }
+//
+//
+//        System.out.println("Precio final" + Descuento);
+
+        System.out.println("Ejercicio5 --------------------------------------------------------");
+//        {
+//        int pares;
+//        int resultado = 0;
+//
+//        do {
+//            pares = scanner.nextInt();
+//            if (pares % 2 == 0) {
+//                resultado = resultado + pares;
+//            }
+//
+//        } while (pares != 0);
+//
+//        System.out.println(resultado);
+//    }
+        System.out.println("Ejercicio6 --------------------------------------------------------");
+
+//        int ceros = 0;
+//        int negativo = 0;
+//        int positivo = 0;
+//        int ingreso = 0;
+//
+//
+//        { for (int i = 1; i < 11; i++) {
+//            System.out.println("Ingrese el número" + i + ":");
+//            ingreso = scanner.nextInt();
+//
+//            if (ingreso < 0){
+//               negativo = negativo + 1;}
+//
+//            if (ingreso == 0){
+//                ceros = ceros + 1;}
+//
+//            if (ingreso > 0){
+//                positivo = positivo + 1;}
+//
+//
+//
+//        }} System.out.println("negativo =" + negativo);
+//        System.out.println("positivo =" + positivo);
+//        System.out.println("ceros =" + ceros);
+//
+
+
+    }
+    }
+
+
